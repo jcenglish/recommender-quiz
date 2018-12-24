49d2dfb4ece76280f4cf98cc1114a8d326c8adaf
Jasmine English <contact@jasmineenglish.com>
2018-12-24 00:05:43 -0500

    ...

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    content = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.content
+
+
+class Role(models.Model):
+    content = models.CharField(max_length=200)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.content
+
+
+class CompanyCulture(models.Model):
+    kind = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.kind
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    size = models.IntegerField()
+    culture = models.ForeignKey(CompanyCulture, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    salary = models.IntegerField()
+    wage = models.IntegerField()
+    date_published = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
