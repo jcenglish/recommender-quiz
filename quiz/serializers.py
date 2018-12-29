35c48e1bd08e8bfd289eeab9aedead7be9d0ce35
Jasmine English <contact@jasmineenglish.com>
2018-12-28 23:09:03 -0500

    added serializers for User and Quiz

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -1,32 +1,15 @@
-# from django.contrib.auth.models import User, Group
-# from quiz.models import Question, Choice, Quiz
-# from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Quiz, User
+from rest_framework import serializers
 
 
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ('url', 'username', 'email', 'groups')
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
 
 
-# class GroupSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Group
-#         fields = ('url', 'name')
-
-
-# class QuizSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Quiz
-#         fields = ('url', 'title', 'description')
-
-# # class QuestionSerializer(serializers.HyperlinkedModelSerializer):
-# #     class Meta:
-# #         model = Question
-# #         fields = ('url', 'content')
-
-
-# # class ChoiceSerializer(serializers.HyperlinkedModelSerializer):
-# #     class Meta:
-# #         model = Choice
-# #         fields = ('url', 'content')
+class QuizSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = ('url', 'title', 'description')
